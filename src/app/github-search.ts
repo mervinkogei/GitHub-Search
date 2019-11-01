706bb53ee10163ed7cbfeb0ba7874b2d0565ba17
mervinkogei <vincent.kogei@yahoo.com>
2019-11-01 12:54:52 +0300

    create githubsearch class

---
+++ src/app/github-search.ts
@@ -0,0 +1,7 @@
+// import { GithubSearchComponent } from './github-search/github-search.component'
+
+export class GithubSearchComponent{
+    constructor(public username:string,public repo:string){
+
+    }
+}
