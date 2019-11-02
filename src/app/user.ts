eb94cce0d807bd7e783a667faa29107cad5b4af3
mervinkogei <vincent.kogei@yahoo.com>
2019-11-02 11:24:59 +0300

    update the class constructor

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    constructor(
+        public username: string,
+        public avatar_url:any,
+        public company: string,
+        public location: string,
+        public followers: any,
+        public following: any,
+        public created_at: Date,
+        public public_repos: number,
+        public html_url: any,
+    ){}
 }
