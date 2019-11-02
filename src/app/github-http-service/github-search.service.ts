fe0a45a4d0a66b7c6f4a30039f895b02801c7b45
mervinkogei <vincent.kogei@yahoo.com>
2019-11-02 23:48:16 +0300

    add the http-services, the token key

--- src/app/github-http-service/github-search.service.ts
+++ src/app/github-http-service/github-search.service.ts
@@ -5,85 +5,18 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { promise } from 'protractor';
 import { resolve} from 'dns';
-import { from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubSearchService {
+  _URL = 'https://api.github.com/users/';
+  token = '?access_token=3cbbddb991c02e7d5a2f0fd4224f9267a5153e5f';
+  constructor(public  http: HttpClient) {
+  }
 
-//   user:User;
-//   repo:Repository;
-//   username: any;
-//   githubSearch: any;
-
-//   constructor( public httpClient:HttpClient) { 
-//     this.user= new User('','','','',0,0,new Date(),0,0);
-//     this.repo = new Repository('','','');
-//   }
-
-//   gitUsername(gitUsername:string){
-//     console.log(this.gitUsername)
-
-//     interface ApiResponse {
-//          avatar_url:any,
-//          username: string,
-//          company: string,
-//          location: string,
-//          followers: number,
-//          following: number,
-//          created_at: Date,
-//          public_repos: number,
-//          html_url: any,
-//     }
-//     let githubSearch = 'https://api.github.com/users/' + gitUsername + '?access_token=' + environment.apiurl;
-//     console.log(this.githubSearch);
-//   }
-// }
-//   let promise = new Promise((resolve, reject) => {
-
-//     this.http.get<ApiResponse>(githubSearch).toPromise().then(
-//       (output) => {
-
-//         console.log(output);
-//         this.user = output;
-//         console.log(this.User);
-
-//         resolve();
-//       },
-//       (error) => {
-//         console.log(error);
-//         reject();
-//       }
-//     );
-//   });
-//   return promise;
-
-// }
-// gitRepo(repo:string) {
-
-//     interface ApiResponse {
-//      name: string;
-//      description: string;
-//      html_url: string;
-     
-//     }
-
-//     let githubSearch = 'https://api.github.com/users/' + gitRepo + '/repos?access_token=' + environment.apiurl;
-
-//     let promise = new Promise((resolve, reject) => {
-//       this.http.get<ApiResponse>(githubSearch).toPromise().then(
-//         (repoOutput) => {
-//           console.log(repoOutput);
-//           this.repo = repoOutput;
-//           resolve();
-//         },
-//         (error) => {
-//           console.log(error);
-//           reject();
-//         }
-//       );
-//     });
-//     return promise;
-//   }
+  getRepo(searchTerm: string): Observable<any> {
+    return this.http.get(this._URL + searchTerm + '/repos?' + this.token);
+  }
 }
