93e837ac8a46f31942466926d5d0167c1a930999
mervinkogei <vincent.kogei@yahoo.com>
2019-11-03 17:18:32 +0300

    add the date-pipr highlight directive

--- src/app/github-http-service/github-search.service.ts
+++ src/app/github-http-service/github-search.service.ts
@@ -1,15 +1,14 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {environment} from '../environments/environment';
-import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
-import {SearchUserComponent} from './search-user/search-user.component';
+// import {environment} from '../environments/environment';
+// import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
 import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GithubSearch {
-  user: GitUsername[] = [];
+export class GitSearch {
+  // user: Username[] = [];
   _URL = 'https://api.github.com/users/';
   token = '?access_token=a7bc763c5aa378e07575b0658605e6f4a8d85a13';
 
